33fc6074e7698bcfb2535a5af8d36bdb7ca00909
keeeyaan <keanjieden@gmail.com>
2024-01-31 20:50:15 +0800

    added login limiter middleware

---
+++ backend/src/middlewares/loginLimiterMiddleware.js
@@ -0,0 +1,23 @@
+import { rateLimit } from "express-rate-limit";
+import logger from "../utils/Logger.js";
+import CustomError from "../utils/CustomError.js";
+
+const loginLimiter = rateLimit({
+  windowMs: 60 * 1000,
+  max: 5,
+  message: {
+    message:
+      "Too many login attemps from this IP, please try again after 60 second pause",
+  },
+  handler: (req, res, next, options) => {
+    logger.error(
+      `Too Many Request: ${options.message.message}\t${req.method}\t${req.url}\t${req.headers.origin}`
+    );
+    next(new CustomError(options.message, options.statusCode));
+    // res.status(options.statusCode).send(options.message);
+  },
+  standardHeaders: true,
+  legacyHeaders: false,
+});
+
+export default loginLimiter;
