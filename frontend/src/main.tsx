d40b94d973ada7b750ee4e40efe38564ccc56cbf
keeeyaan <keanjieden@gmail.com>
2024-02-08 01:17:20 +0800

    added queryClient default options and toaster

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,14 +3,25 @@ import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import App from "./App.tsx";
+import { Toaster } from "@/components/ui/toaster";
 import "./index.css";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      refetchOnMount: false,
+      refetchOnReconnect: false,
+      retry: 1,
+    },
+  },
+});
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
+      <Toaster />
     </QueryClientProvider>
   </React.StrictMode>
 );
