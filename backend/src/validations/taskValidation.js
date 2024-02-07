88408df3ae9755b813f13e1aa13adaeda97007f2
keeeyaan <keanjieden@gmail.com>
2024-02-08 01:14:32 +0800

    added create and update task validation

---
+++ backend/src/validations/taskValidation.js
@@ -0,0 +1,15 @@
+import { body, param } from "express-validator";
+import { mapValidationErrors } from "../middlewares/validationMiddleware.js";
+
+export const validateCreateUpdateTask = mapValidationErrors([
+  param("taskId").notEmpty().withMessage("Task ID is required!"),
+  body("title").notEmpty().withMessage("Task title is required!"),
+  body("description")
+    .notEmpty()
+    .withMessage("Task description is required!")
+    .isLength({ max: 1024 })
+    .withMessage("Task description must be under 1024 characters."),
+  body("priority"),
+  body("status"),
+  body("tag"),
+]);
