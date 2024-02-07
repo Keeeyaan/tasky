ed664dc14adb7d91ec0690c6b799e3587c70667d
keeeyaan <keanjieden@gmail.com>
2024-02-08 01:15:15 +0800

    added createTask and updateTaskById route

--- backend/src/routes/taskRouter.js
+++ backend/src/routes/taskRouter.js
@@ -1,15 +1,18 @@
 import { Router } from "express";
 
+import { createTask, updateTaskById } from "../controllers/taskController.js";
+import { validateCreateUpdateTask } from "../validations/taskValidation.js";
+
 const router = Router();
 
 router
   .route("/")
   .get(() => {})
-  .post(() => {});
+  .post(validateCreateUpdateTask, createTask);
 
 router
   .route("/:taskId")
-  .patch(() => {})
+  .patch(validateCreateUpdateTask, updateTaskById)
   .delete(() => {});
 
 export default router;
