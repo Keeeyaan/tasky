07ba0db93a763cb7fdb3e87337436b6c448fec7d
keeeyaan <keanjieden@gmail.com>
2024-02-09 13:11:03 +0800

    added update task by id custom hooks

---
+++ frontend/src/hooks/useUpdateTaskById.ts
@@ -0,0 +1,33 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+
+import { useToast } from "@/components/ui/use-toast";
+
+import { updateTaskById } from "@/api/task";
+
+export const useUpdateTaskId = () => {
+  const queryClient = useQueryClient();
+
+  const { toast } = useToast();
+
+  return useMutation({
+    mutationKey: ["task"],
+    mutationFn: updateTaskById,
+    onSuccess: (data) => {
+      queryClient.invalidateQueries({
+        queryKey: ["task"],
+      });
+      toast({
+        title: "Task Updated!",
+        description: data.message,
+      });
+    },
+    onError: (error: any) => {
+      toast({
+        title: "Opps.. Error on update. Try again!",
+        description: error.response.data.message || error.message,
+      });
+    },
+  });
+};
+
+export default useUpdateTaskId;
