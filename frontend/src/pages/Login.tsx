7bae79812d6c0b5536a8218797a746b3d2743495
keeeyaan <keanjieden@gmail.com>
2024-02-09 13:09:36 +0800

    seprate the logic for form login

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,82 +1,45 @@
-import { z } from "zod";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
+import { Link } from "react-router-dom";
 
+import LoginForm from "@/components/form/LoginForm";
+import logo from "@/assets/logo.png";
 import Wrapper from "@/components/Wrapper";
-import { LoginValidationSchema } from "@/utils/ValidationSchema";
-import { Button } from "@/components/ui/button";
-import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
 import {
   Card,
   CardContent,
   CardDescription,
+  CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
 
 const Login = () => {
-  const form = useForm<z.infer<typeof LoginValidationSchema>>({
-    resolver: zodResolver(LoginValidationSchema),
-    defaultValues: {
-      email: "",
-      password: "",
-    },
-  });
-
-  function onSubmit(values: z.infer<typeof LoginValidationSchema>) {
-    console.log(values);
-  }
-
   return (
-    <Wrapper className="h-screen flex justify-center items-center">
+    <Wrapper
+      title="Login"
+      className="h-screen flex justify-center items-center"
+    >
       <Card className="w-[400px]">
-        <CardHeader className="text-center">
+        <CardHeader>
+          <div className="flex justify-center items-center gap-2 mb-2">
+            <img src={logo} className="w-[50px] h-[50px] mb-2" />
+            <h1 className="font-semibold text-3xl">Tasky</h1>
+          </div>
           <CardTitle>Login</CardTitle>
-          <CardDescription>Login to </CardDescription>
+          <CardDescription>
+            Enter your email and password to login.
+          </CardDescription>
         </CardHeader>
         <CardContent>
-          <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
-              <FormField
-                control={form.control}
-                name="email"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Email</FormLabel>
-                    <FormControl>
-                      <Input placeholder="Email" {...field} />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <FormField
-                control={form.control}
-                name="password"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Password</FormLabel>
-                    <FormControl>
-                      <Input placeholder="Password" {...field} />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <Button className="w-full" type="submit">
-                Submit
-              </Button>
-            </form>
-          </Form>
+          <LoginForm />
         </CardContent>
+        <CardFooter className="flex justify-center">
+          <Link to="/register" className="text-xs cursor-default">
+            Don't have an account yet?{" "}
+            <span className=" text-primary hover:underline cursor-pointer">
+              Register
+            </span>
+          </Link>
+        </CardFooter>
       </Card>
     </Wrapper>
   );
