e6c21bce583027635dd32496004ac9e1f48d709b
keeeyaan <keanjieden@gmail.com>
2024-02-10 15:33:11 +0800

    added dnd helper functions

---
+++ frontend/src/helper/dnd.tsx
@@ -0,0 +1,31 @@
+import { DraggableLocation } from "react-beautiful-dnd";
+
+import { ITask } from "@/types";
+
+export const reorder = (
+  list: ITask[],
+  startIndex: number,
+  endIndex: number
+) => {
+  const result = [...list];
+  const [removed] = result.splice(startIndex, 1);
+  result.splice(endIndex, 0, removed);
+  return result;
+};
+
+export const move = (
+  source: ITask[],
+  destination: ITask[],
+  droppableSource: DraggableLocation,
+  droppableDestination: DraggableLocation
+) => {
+  const sourceClone = [...source];
+  const destClone = [...destination];
+  const [removed] = sourceClone.splice(droppableSource.index, 1);
+  destClone.splice(droppableDestination.index, 0, removed);
+
+  return {
+    [droppableSource.droppableId]: sourceClone,
+    [droppableDestination.droppableId]: destClone,
+  };
+};
