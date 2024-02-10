a8f8f99cf71c6f046bdc58fb7972b60b98ffd3e8
keeeyaan <keanjieden@gmail.com>
2024-02-10 15:31:23 +0800

    added topbar

---
+++ frontend/src/components/Topbar.tsx
@@ -0,0 +1,38 @@
+import { Bell, Calendar, PanelRightOpen } from "lucide-react";
+
+import { useStore } from "@/store";
+
+import SearchInput from "./SearchInput";
+import { Card } from "./ui/card";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import { Button } from "./ui/button";
+
+const Topbar = () => {
+  const { setSidebarIsClosed } = useStore();
+
+  return (
+    <Card className="sticky inset-x-0 top-0 z-30 border-t-0 border-x-0 w-full rounded-none backdrop-blur-lg transition-all">
+      <div className="w-full h-full flex items-center px-6 py-5 justify-between">
+        <div className="flex items-center gap-2">
+          <Button variant="ghost" size="sm" className="px-2">
+            <PanelRightOpen onClick={setSidebarIsClosed} />
+          </Button>
+          <SearchInput type="text" placeholder="Search" />
+        </div>
+        <div className="flex items-center gap-4">
+          <Bell size={20} />
+          <Calendar size={20} />
+          <div className="flex items-center gap-2">
+            <Avatar>
+              <AvatarImage src="https://github.com/shadcn.png" />
+              <AvatarFallback>CN</AvatarFallback>
+            </Avatar>
+            <h3 className="text-sm font-medium">Villaflor</h3>
+          </div>
+        </div>
+      </div>
+    </Card>
+  );
+};
+
+export default Topbar;
