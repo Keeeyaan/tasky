cd3e04ea1eb4c2f316585344cbbb859569b24f1c
keeeyaan <keanjieden@gmail.com>
2024-02-13 00:17:42 +0800

    added user profile dropdown component

--- frontend/src/components/Topbar.tsx
+++ frontend/src/components/Topbar.tsx
@@ -2,9 +2,9 @@ import { Bell, Calendar, PanelRightOpen, PanelLeftOpen } from "lucide-react";
 
 import { useStore } from "@/store";
 
+import UserProfile from "./UserProfile";
 import SearchInput from "./SearchInput";
 import { Card } from "./ui/card";
-import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 import { Button } from "./ui/button";
 
 const Topbar = () => {
@@ -14,7 +14,7 @@ const Topbar = () => {
     <Card className="sticky inset-x-0 top-0 z-30 border-t-0 border-x-0 w-full rounded-none backdrop-blur-lg transition-all">
       <div className="w-full h-full flex items-center px-6 py-5 justify-between">
         <div className="flex items-center gap-2">
-          <Button variant="ghost" size="sm" className="px-2">
+          <Button variant="ghost" size="sm" className="px-2 text-gray-800">
             {sidebarIsClosed ? (
               <PanelLeftOpen onClick={setSidebarIsClosed} />
             ) : (
@@ -23,16 +23,10 @@ const Topbar = () => {
           </Button>
           <SearchInput type="text" placeholder="Search" />
         </div>
-        <div className="flex items-center gap-4">
+        <div className="flex items-center gap-5 text-gray-800">
           <Bell size={20} />
           <Calendar size={20} />
-          <div className="flex items-center gap-2">
-            <Avatar>
-              <AvatarImage src="https://github.com/shadcn.png" />
-              <AvatarFallback>CN</AvatarFallback>
-            </Avatar>
-            <h3 className="text-sm font-medium">Villaflor</h3>
-          </div>
+          <UserProfile />
         </div>
       </div>
     </Card>
