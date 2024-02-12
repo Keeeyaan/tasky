c92031756ad1730cacb1ebc3271809a061495ca4
keeeyaan <keanjieden@gmail.com>
2024-02-12 22:42:07 +0800

    added footer

---
+++ frontend/src/components/Footer.tsx
@@ -0,0 +1,28 @@
+import { useLocation } from "react-router-dom";
+
+import { CardDescription, CardHeader } from "./ui/card";
+
+const ROUTE = ["/dashboard"];
+
+const Footer = () => {
+  const { pathname } = useLocation();
+
+  // hides the header in admin pages
+  if (ROUTE.includes(pathname)) return;
+
+  const currentYear = new Date().getFullYear();
+
+  return (
+    <footer className="w-full">
+      <div className="rounded-none bg-primary text-center w-full">
+        <CardHeader className="p-4">
+          <CardDescription className="text-white">
+            &copy; {currentYear} Kean Jieden Villaflor. All rights reserved.
+          </CardDescription>
+        </CardHeader>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
