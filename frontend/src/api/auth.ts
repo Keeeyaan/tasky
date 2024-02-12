702c31da7ad4dcd2b68cab1a2dc8c46e4e312b95
keeeyaan <keanjieden@gmail.com>
2024-02-12 22:40:23 +0800

    remove axiosPrivate interceptor

--- frontend/src/api/auth.ts
+++ frontend/src/api/auth.ts
@@ -5,24 +5,18 @@ axios.defaults.withCredentials = true;
 
 import {
   ILoginRefreshResponse,
-  IRegisterResponse,
+  IGenericResponse,
   IRegisterProp,
   ILoginProp,
 } from "@/types";
 
-export const axiosPrivate = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
-  withCredentials: true,
-  headers: { "Content-Type": "application/json" },
-});
-
 export async function login(data: ILoginProp) {
-  const response = await axios.post<ILoginRefreshResponse>(`auth/login`, data);
+  const response = await axios.post<IGenericResponse>(`auth/login`, data);
   return response.data;
 }
 
 export async function register(data: IRegisterProp) {
-  const response = await axios.post<IRegisterResponse>(`auth/register`, data);
+  const response = await axios.post<IGenericResponse>(`auth/register`, data);
   return response.data;
 }
 
@@ -30,24 +24,3 @@ export async function refreshAccessToken() {
   const response = await axios.get<ILoginRefreshResponse>("auth/refresh");
   return response.data;
 }
-
-axiosPrivate.interceptors.response.use(
-  (response) => {
-    return response;
-  },
-  async (error) => {
-    const prevRequest = error?.config;
-    const errorMessage = error.response?.data.message;
-
-    if (errorMessage?.includes("Unathorized") && !prevRequest?.sent) {
-      prevRequest.sent = true;
-
-      const newAccessToken = await refreshAccessToken();
-      prevRequest.headers[
-        "Authorization"
-      ] = `Bearer ${newAccessToken.accessToken}`;
-      return axiosPrivate(prevRequest);
-    }
-    return Promise.reject(error);
-  }
-);
