26ec38d51a510ce97b0d20edb73df8da7074d4fa
keeeyaan <keanjieden@gmail.com>
2024-02-13 00:54:31 +0800

    set accessToken to empty string

--- frontend/src/hooks/useLogoutUser.ts
+++ frontend/src/hooks/useLogoutUser.ts
@@ -4,11 +4,12 @@ import { useNavigate } from "react-router-dom";
 import { useToast } from "@/components/ui/use-toast";
 
 import { logout } from "@/api/auth";
+import { useStore } from "@/store";
 
 const useLogoutUser = () => {
   const queryClient = useQueryClient();
   const navigate = useNavigate();
-
+  const { setAuth } = useStore();
   const { toast } = useToast();
 
   return useMutation({
@@ -16,6 +17,7 @@ const useLogoutUser = () => {
     mutationFn: logout,
     onSuccess: (data) => {
       queryClient.removeQueries();
+      setAuth({ accessToken: "" });
       toast({
         title: "Logout Success",
         description: data.message,
