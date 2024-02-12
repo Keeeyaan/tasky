5c85e4838082bc80519e2918854a5d5322d717f9
keeeyaan <keanjieden@gmail.com>
2024-02-12 22:42:14 +0800

    added navbar

---
+++ frontend/src/components/Navbar.tsx
@@ -0,0 +1,38 @@
+import { Link } from "react-router-dom";
+
+import { useLocation } from "react-router-dom";
+import logo from "@/assets/logo.png";
+
+const ROUTE = ["/dashboard"];
+
+const Navbar = () => {
+  const { pathname } = useLocation();
+
+  // hides the header in main pages
+  if (ROUTE.includes(pathname)) return;
+
+  return (
+    <nav className="sticky h-16 inset-x-0 top-0 z-30 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all">
+      <div
+        className="md:px-40
+         w-full mx-auto px-4  max-screen-xl"
+      >
+        <div className="flex h-16 items-center justify-between border-b border-zinc-200">
+          <Link
+            to="/"
+            className="flex z-40 font-bold text-2xl items-center gap-2"
+          >
+            <img
+              className="w-[40px] h-[40px]"
+              alt="linkedlist logo"
+              src={logo}
+            />
+            <span>Tasky</span>
+          </Link>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
