de3b9dea154b7b2d0a2e385823eb56a9b6d5bfcb
keeeyaan <keanjieden@gmail.com>
2024-02-12 23:01:55 +0800

    added DashboardLayout and Task page on index exports

--- frontend/src/pages/index.ts
+++ frontend/src/pages/index.ts
@@ -2,4 +2,6 @@ export { default as Layout } from "./Layout";
 export { default as Landing } from "./Landing";
 export { default as Login } from "./Login";
 export { default as Register } from "./Register";
+export { default as DashboardLayout } from "./DashboardLayout";
+export { default as Task } from "./Task";
 export { default as Error } from "./Error";
