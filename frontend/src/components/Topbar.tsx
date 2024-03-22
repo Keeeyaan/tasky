d9966196e50aa2c576c6d2d2476d3a924500a726
keeeyaan <keanjieden@gmail.com>
2024-03-22 14:29:17 +0800

    update topbar ui

--- frontend/src/components/Topbar.tsx
+++ frontend/src/components/Topbar.tsx
@@ -1,31 +1,35 @@
-import { Bell, Calendar, PanelRightOpen, PanelLeftOpen } from "lucide-react";
-
-import { useStore } from "@/store";
+import { Bell, Calendar } from "lucide-react";
 
 import UserProfile from "./UserProfile";
 import SearchInput from "./SearchInput";
 import { Card } from "./ui/card";
-import { Button } from "./ui/button";
+import { Link } from "react-router-dom";
 
 const Topbar = () => {
-  const { setSidebarIsClosed, sidebarIsClosed } = useStore();
-
   return (
     <Card className="sticky inset-x-0 top-0 z-30 border-t-0 border-x-0 w-full rounded-none backdrop-blur-lg transition-all">
-      <div className="w-full h-full flex items-center px-6 py-5 justify-between">
-        <div className="flex items-center gap-2">
-          <Button variant="ghost" size="sm" className="px-2 text-gray-800">
-            {sidebarIsClosed ? (
-              <PanelLeftOpen onClick={setSidebarIsClosed} />
-            ) : (
-              <PanelRightOpen onClick={setSidebarIsClosed} />
-            )}
-          </Button>
-          <SearchInput type="text" placeholder="Search" />
+      <div className="w-full h-full flex items-center px-40 py-6 justify-between">
+        <div className="flex items-center gap-6">
+          <Link
+            to="/"
+            className="flex z-40 font-bold text-2xl items-center gap-4"
+          >
+            <img
+              className="w-[40px] h-[40px]"
+              alt="linkedlist logo"
+              src="/logo.png"
+            />
+            <span>Tasky</span>
+          </Link>
         </div>
-        <div className="flex items-center gap-5 text-gray-800">
-          <Bell size={20} />
-          <Calendar size={20} />
+        <div className="flex items-center gap-6 text-gray-800">
+          <SearchInput type="text" placeholder="Search" />
+          <div className="p-2 border rounded-full">
+            <Bell size={20} />
+          </div>
+          <div className="p-2 border rounded-full">
+            <Calendar size={20} />
+          </div>
           <UserProfile />
         </div>
       </div>
