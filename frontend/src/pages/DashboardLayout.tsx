754d76bd9230fa5b9a64c7735e7ba2c9062f478c
keeeyaan <keanjieden@gmail.com>
2024-03-22 14:30:39 +0800

    added headmenu

--- frontend/src/pages/DashboardLayout.tsx
+++ frontend/src/pages/DashboardLayout.tsx
@@ -1,14 +1,15 @@
 import { Outlet } from "react-router-dom";
 
-import Sidebar from "@/components/Sidebar";
 import Topbar from "@/components/Topbar";
+import HeadMenu from "@/components/HeadMenu";
 
 const DashboardLayout = () => {
   return (
     <main className="flex min-h-screen">
-      <Sidebar />
+      {/* <Sidebar /> */}
       <div className="w-full space-y-4">
         <Topbar />
+        <HeadMenu />
         <Outlet />
       </div>
     </main>
