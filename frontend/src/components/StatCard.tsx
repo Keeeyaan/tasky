4cbacca1f84b5fbbb7bd7eb022ac8ab626ecb160
keeeyaan <keanjieden@gmail.com>
2024-03-22 14:29:35 +0800

    update statcard ui

--- frontend/src/components/StatCard.tsx
+++ frontend/src/components/StatCard.tsx
@@ -1,31 +1,36 @@
-import { cn } from "@/lib/utils";
+import { ReactNode } from "react";
 
 import { Card } from "./ui/card";
+import { cn } from "@/lib/utils";
 
 const StatCard = ({
   title,
   total,
+  icon,
   className,
 }: {
   title: string;
   total: number;
+  icon: ReactNode;
   className: string;
 }) => {
   return (
     <Card
-      className={cn(
-        "shadow-none border-none p-4 min-w-[130px] bg-gradient-to-r text-white",
-        className
-      )}
+      className={cn("p-4 min-w-[200px] bg-gradient-to-r text-white", className)}
     >
-      <div className="font-medium uppercase flex items-center justify-between">
+      <div className="font-medium text-lg flex items-center gap-2 mb-4">
+        <div className="relative">
+          <div className="p-4 rounded bg-gradient-to-tr shadow border opacity-80 to-white/70 from-transparent" />
+          <div className="text-white absolute transform -translate-x-1/2 -translate-y-1/2 top-2/4 left-2/4">
+            {icon}
+          </div>
+        </div>
         <h1>{title}</h1>
       </div>
-
-      <div className="flex mt-1 items-center gap-1">
-        <div className="bg-green-200 text-xs rounded-full h-[30px] px-0.5 font-semibold" />
-        <h2 className="text-foreground font-semibold text-3xl">{total}</h2>
-      </div>
+      <h2 className="font-medium text-2xl flex">
+        <span className="bg-green-200 rounded-full px-0.5 mr-1" />
+        {total}
+      </h2>
     </Card>
   );
 };
