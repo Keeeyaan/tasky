a3293e1ce8577d5030079f6804b56ee91422d57a
keeeyaan <keanjieden@gmail.com>
2024-05-12 15:02:53 +0800

    adjusted size for smaller screen

--- frontend/src/components/HeadMenu.tsx
+++ frontend/src/components/HeadMenu.tsx
@@ -9,7 +9,6 @@ import {
 } from "@/components/ui/card";
 import { Separator } from "@/components/ui/separator";
 import { Button, buttonVariants } from "@/components/ui/button";
-import TaskCreateButton from "@/components/task/TaskCreateButton";
 import Wrapper from "./Wrapper";
 
 const HeadMenu = () => {
@@ -18,10 +17,12 @@ const HeadMenu = () => {
   return (
     <Wrapper>
       <Card className="mb-6 w-full">
-        <CardHeader className="pb-4">
-          <CardTitle className="text-4xl font-semibold">Tasks</CardTitle>
+        <CardHeader className="p-4 md:px-6 md:pt-6">
+          <CardTitle className="text-3xl md:text-4xl font-semibold">
+            Tasks
+          </CardTitle>
         </CardHeader>
-        <CardContent className="flex justify-between">
+        <CardContent className="flex justify-between px-4 pb-4 md:px-6 md:pb-6">
           <div className="space-x-2">
             <Link
               to="/task/analytic"
@@ -36,36 +37,13 @@ const HeadMenu = () => {
               Board
             </Link>
           </div>
-          <div className="space-x-2">
+          <div className="hidden sm:block space-x-2">
             <Button variant="outline">Filter</Button>
             <Button variant="outline">&lt; Jun, 2022 &gt;</Button>
           </div>
         </CardContent>
         <Separator />
-        <CardFooter className="py-4 block">
-          {pathname === "/task" && (
-            <div className="grid grid-cols-3 gap-2">
-              <div className="flex items-center justify-between">
-                <h1 className="py-1 px-2 bg-lime-500 rounded font-medium text-sm text-white">
-                  Started
-                </h1>
-                <TaskCreateButton />
-              </div>
-              <div className="flex items-center justify-between">
-                <h1 className="py-1 px-2 bg-yellow-500 rounded font-medium text-sm text-white">
-                  In Progress
-                </h1>
-                <TaskCreateButton />
-              </div>
-              <div className="flex items-center justify-between">
-                <h1 className="py-1 px-2 bg-purple-400 rounded font-medium text-sm text-white">
-                  Completed
-                </h1>
-                <TaskCreateButton />
-              </div>
-            </div>
-          )}
-        </CardFooter>
+        <CardFooter className="py-2 block"></CardFooter>
       </Card>
     </Wrapper>
   );
